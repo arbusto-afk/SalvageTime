71beb76ec947574bf2cdcea86b7337cd58f4603d
Arbusto <88610004+arbusto-afk@users.noreply.github.com>
2021-12-17 13:09:41 -0300

    Add files via upload

---
+++ SalvageTime/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+
+namespace SalvageTime
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public int SalvageTime;
+        public string DiscordSupport;
+        public void LoadDefaults()
+        {
+            SalvageTime = 1;
+            DiscordSupport = "Arbusto#6794";
+        }
+    }
+}
